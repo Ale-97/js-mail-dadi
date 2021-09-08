205971f951994d735470ad96ffcfeb50c68da8ec
Ale-97 <oissela999@gmail.com>
2021-09-08 11:52:20 +0200

    1.2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+var od = document.getElementById;
 var email = prompt('Salve, inserisci la tua mail per poter iniziare il gioco');
+
+const sleep = (s) => {
+    return new Promise(resolve => setTimeout(resolve, (s*1000)))
+  }
 
 var listEmail = ["alessio@gmail.com","francesco@tiscali.com"];
 
@@ -30,5 +35,22 @@ for(var i = 1; i > check; i++ ){
 
 sessionStorage.setItem("dataEmail", email);
 document.getElementById("myEmail").innerHTML = email;
-console.log(email);
-console.log(listEmail);
+
+
+var playerOne = Math.floor(Math.random() * 6 + 1);
+
+var playerTwo = Math.floor(Math.random() * 6 + 1);
+
+document.getElementById("myNumber").innerHTML = 'Il tuo numero è: ' + playerOne;
+document.getElementById("computerNumber").innerHTML = 'Il numero del Computer è: ' + playerTwo;
+
+if(myNumber > computerNumber) {
+    document.getElementById("winner").innerHTML = 'HAI VINTO!';
+}
+else{
+     document.getElementById("winner").innerHTML = 'HAI PERSO!';
+}
+
+sleep(2).then(() => { 
+  var startOver = alert('Vuoi risfidare il computer ? ricarica la pagina');
+})
